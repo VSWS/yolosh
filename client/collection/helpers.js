7b11edbc3dc051639e7a350c8ba160313adf8b68
NgaNguyenDuy <kyo1508@gmail.com>
2015-04-01 04:56:35 +0700

    get data success

--- client/collection/helpers.js
+++ client/collection/helpers.js
@@ -4,12 +4,23 @@
 
 
 if (Meteor.isClient) {
+    
+    Meteor.subscribe('listCollect');
+    
 	// This code only runs on the client
 	Template.collections.helpers({
 		collections: function () {
 			// If hide completed is checked, filter tasks
 			return Collections.find();
 		},
+        ngas: function() {
+            return Collections.find();
+        },
+        tasks: [
+            {title: 'hehe'},
+            {title: 'hihi'},
+            {title: 'haha'}
+        ],
 		incompleteCount: function () {
 			return Collections.find().count();
 		}
